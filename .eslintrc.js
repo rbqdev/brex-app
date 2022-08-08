068b6f14b49a89b28e501ad06dc8262bcf6532c8
Robson Braga de Queiroz <rbq.dev@gmail.com>
2022-08-07 22:27:21 -0300

    chore: add some configs: - eslint + prettier rules - absolute paths with: 'src/*' - react-native font-icons support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['react', 'simple-import-sort'],
+  rules: {
+    // increase the severity of rules so they are auto-fixable
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
 };
