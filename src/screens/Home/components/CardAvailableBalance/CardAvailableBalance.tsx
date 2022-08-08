e088d43e9522b31c9a07c0b8eff0e9b857a8fbfc
Robson Braga de Queiroz <rbq.dev@gmail.com>
2022-08-08 09:28:30 -0300

    refactor: split card on small components

--- src/screens/Home/components/CardAvailableBalance/CardAvailableBalance.tsx
+++ src/screens/Home/components/CardAvailableBalance/CardAvailableBalance.tsx
@@ -5,27 +5,61 @@ import IonIcon from 'react-native-vector-icons/Ionicons';
 import MCIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { Card } from 'src/components';
 
+const CardHeader = () => (
+  <Card.Header paddingBottom={0.1}>
+    <HStack justifyContent="space-between" alignItems="center">
+      <Text fontWeight="bold" fontSize={16}>
+        Available balance
+      </Text>
+      <Box>
+        <Button
+          variant="unstyled"
+          p={0}
+          fontSize={12}
+          justifyContent="flex-end">
+          <Text fontWeight="bold" color="primary.300">
+            View <IonIcon name="arrow-forward-outline" />
+          </Text>
+        </Button>
+      </Box>
+    </HStack>
+  </Card.Header>
+);
+
+const CardFooter = () => (
+  <Card.Footer>
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      overflow="visible">
+      <HStack space={2}>
+        <Button
+          variant="outline"
+          startIcon={<MCIcon name="cash-plus" size={26} />}>
+          <Flex flexDirection="row" alignItems="center">
+            <Text fontSize={16} fontWeight="bold">
+              Deposit check
+            </Text>
+          </Flex>
+        </Button>
+        <Button
+          variant="outline"
+          startIcon={<IonIcon name="arrow-forward-circle-outline" size={26} />}>
+          <Flex flexDirection="row" alignItems="center">
+            <Text fontSize={16} fontWeight="bold">
+              Send payment
+            </Text>
+          </Flex>
+        </Button>
+      </HStack>
+    </ScrollView>
+  </Card.Footer>
+);
+
 export const CardAvailableBalance = () => {
   return (
     <Card hasDividers={false}>
-      <Card.Header paddingBottom={0.1}>
-        <HStack justifyContent="space-between" alignItems="center">
-          <Text fontWeight="bold" fontSize={16}>
-            Available balance
-          </Text>
-          <Box>
-            <Button
-              variant="unstyled"
-              p={0}
-              fontSize={12}
-              justifyContent="flex-end">
-              <Text fontWeight="bold" color="primary.300">
-                View <IonIcon name="arrow-forward-outline" />
-              </Text>
-            </Button>
-          </Box>
-        </HStack>
-      </Card.Header>
+      <CardHeader />
 
       <Card.Content paddingTop={1} paddingBottom={0.1}>
         <Text fontSize={32} fontWeight="bold">
@@ -33,35 +67,7 @@ export const CardAvailableBalance = () => {
         </Text>
       </Card.Content>
 
-      <Card.Footer>
-        <ScrollView
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          overflow="visible">
-          <HStack space={2}>
-            <Button
-              variant="outline"
-              startIcon={<MCIcon name="cash-plus" size={26} />}>
-              <Flex flexDirection="row" alignItems="center">
-                <Text fontSize={16} fontWeight="bold">
-                  Deposit check
-                </Text>
-              </Flex>
-            </Button>
-            <Button
-              variant="outline"
-              startIcon={
-                <IonIcon name="arrow-forward-circle-outline" size={26} />
-              }>
-              <Flex flexDirection="row" alignItems="center">
-                <Text fontSize={16} fontWeight="bold">
-                  Send payment
-                </Text>
-              </Flex>
-            </Button>
-          </HStack>
-        </ScrollView>
-      </Card.Footer>
+      <CardFooter />
     </Card>
   );
 };
